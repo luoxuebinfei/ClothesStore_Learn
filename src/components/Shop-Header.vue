9967f7129e4500d3c9ce18a3c4b2b4d0859f0664
落雪 <1732806519@qq.com>
2022-03-08 20:32:31 +0800

    添加购物车路径

--- src/components/Shop-Header.vue
+++ src/components/Shop-Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header clearfix">
-    <div class="search">
+    <div class="search clearfix">
       <!-- 搜索相关 -->
       <el-form ref="form" :model="form">
         <el-input v-model="form.searchText" placeholder="搜索"
@@ -12,12 +12,12 @@
         ></el-input>
       </el-form>
     </div>
-    <div class="shortcut">
+    <div class="shortcut clearfix">
       <ul>
-        <li><el-button icon="iconfont icon-wode" title="个人中心"></el-button></li>
+        <li><a href="/home"><el-button icon="iconfont icon-wode" title="个人中心"></el-button></a></li>
         <li><el-button icon="iconfont icon-dingdan" title="订单"></el-button></li>
         <!-- <li><el-button icon="iconfont icon-wode" title="个人中心"></el-button></li> -->
-        <li><el-button icon="iconfont icon-gouwuche" title="购物车"></el-button></li>
+        <li><a href="/cart_index"><el-button icon="iconfont icon-gouwuche" title="购物车"></el-button></a></li>
       </ul>
     </div>
   </div>
@@ -38,20 +38,29 @@ export default {
       console.log(`提交了 ${this.form.searchText}`);
     },
   },
+  watch: {
+    $route() {
+      this.$router.go(0);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .header {
   padding: 1rem;
+  width: 100%;
+  min-width: 1366px;
+  box-shadow: 1px 1px 15px rgb(155, 155, 155);
+  // border-bottom: 1px solid gray;
 }
 /*搜索相关样式 */
 .search {
   width: 35rem;
-  margin: 0 auto;
+  margin: 0 3rem 0 20rem;
   float: left;
-  position: relative;
-  left: 20rem;
+  // position: relative;
+  // left: 20rem;
 }
 .search /deep/ .el-input__inner {
   border-radius: 15px;
@@ -73,11 +82,18 @@ export default {
 /**************/
 
 /*快捷路径*/
+.shortcut{
+  width: 336px;
+  // position: relative;
+  // top: -5px;
+  // right: 10rem;
+  float: right;
+  margin: 0 4rem 0 0;
+}
 .shortcut ul>li{
   list-style: none;
   float: right;
-  position: relative;
-  right: 10rem;
+  
   margin: 0 1.5rem;
 }
 .shortcut /deep/ .el-button,.el-button:hover,.el-button:active{
