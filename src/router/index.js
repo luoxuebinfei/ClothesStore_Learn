73cc731a3982bd2428fd4a9a06b5a3701d0d6745
落雪 <1732806519@qq.com>
2022-03-11 04:54:04 +0800

    添加新路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,31 +3,49 @@ import VueRouter from 'vue-router'
 import Index from "../views/Index";
 import Login from "../views/Login";
 import Register from "../views/register";
+import UserHome from "../views/UserHome";
+import Cart_index from "../views/Cart_index";
+import ShopInfo from "../views/ShopInfo";
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'index',
-    component:Index
+    component: Index,
   },
   {
     path: '/login',
     name: 'login',
-    component:Login
+    component: Login
   },
   {
     path: '/register',
     name: 'register',
     component: Register
   },
+  {
+    path: '/home',
+    name: 'home',
+    component: UserHome
+  },
+  {
+    path: '/cart_index',
+    name: 'cart_index',
+    component:Cart_index
+  },
+  {
+    path: '/shopinfo/:id',
+    name: 'shopInfo',
+    component:ShopInfo
+  }
 ]
-  
+
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes:routes
 })
-  
-  export default router
+
+export default router
