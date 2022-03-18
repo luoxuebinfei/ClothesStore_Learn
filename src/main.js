cecfcbd1f49e50fad9ed03dd6ff5490ff14daba9
落雪 <1732806519@qq.com>
2022-03-18 14:29:07 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -7,14 +7,20 @@ import router from './router/index'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import './assets/CSS/tailwind.css'
-import './assets/CSS/global.css'
+import './assets/CSS/tailwind.css';
+import './assets/CSS/global.css';
 //引入图标样式
-import './assets/icon/icon1/iconfont.css'
+import './assets/icon/icon1/iconfont.css';
+
+//引入懒加载方法
+import LazyLoad from "@/assets/JS/LazyLoad";
 
 
 
 Vue.use(ElementUI);
+Vue.use(LazyLoad,{
+  default: 'xxx.png',
+});
 Vue.config.productionTip = false
 new Vue({
   router,
